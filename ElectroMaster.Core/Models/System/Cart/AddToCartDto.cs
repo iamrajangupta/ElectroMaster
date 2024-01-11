60049e9e616925dbc98d7b6245f7734dee0a39c4
Rajan Gupta <rajan.gupta@etevatech.com>
2024-01-11 21:26:53 +0530

    sd

---
+++ ElectroMaster.Core/Models/System/Cart/AddToCartDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectroMaster.Core.Models.System.Cart
+{
+    public class AddToCartDto
+    {
+        public string ProductReference { get; set; }
+        public string ProductVariantReference { get; set; }
+        public string ProductName { get; set; }
+        public int ProductCount { get; set; }
+
+    }
+}
